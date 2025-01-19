07ffc209b362b3adbafe6d837eae957e1785e2eb
Amorto Goon <amorto.goon.121@gmail.com>
2025-01-19 14:13:46 +0600

    feat: configure TypeScript and ESLint to ignore build errors

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default withPayload(nextConfig)
