418f8ffd695ef903508d5caa528c3c80261094eb
Amorto Goon <amorto.goon.121@gmail.com>
2025-02-23 16:03:46 +0600

    feat: enhance revalidatePage hook to support localization for revalidation

--- src/collections/Pages/hooks/revalidate-page.ts
+++ src/collections/Pages/hooks/revalidate-page.ts
@@ -1,3 +1,4 @@
+import { locales } from '@/const/locale'
 import { revalidatePath } from 'next/cache'
 import { CollectionAfterChangeHook } from 'payload'
 
@@ -5,12 +6,24 @@ const getPathFromSlug = (slug: string) => {
   return slug === 'home' ? '/' : `/${slug}`
 }
 
-export const revalidatePage: CollectionAfterChangeHook = ({ doc, previousDoc }) => {
+export const revalidatePage: CollectionAfterChangeHook = ({
+  doc,
+  previousDoc,
+  req: { payload },
+}) => {
   if (doc._status === 'published') {
-    revalidatePath(getPathFromSlug(doc.slug))
+    payload.logger.info(`Revalidating page: ${doc.slug}`)
+
+    locales.forEach((locale) => {
+      revalidatePath(`${locale}${getPathFromSlug(doc.slug)}`)
+    })
   }
 
   if (previousDoc?._status === 'published' && doc._status !== 'published') {
-    revalidatePath(getPathFromSlug(doc.slug))
+    payload.logger.info(`Revalidating page: ${doc.slug}`)
+
+    locales.forEach((locale) => {
+      revalidatePath(`${locale}${getPathFromSlug(doc.slug)}`)
+    })
   }
 }
