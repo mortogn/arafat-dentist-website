2d7472a39cb2599a77fe2b7819029319fa643f45
Amorto Goon <amorto.goon.121@gmail.com>
2025-03-12 16:39:15 +0600

    feat: enhance StatsSection layout and typography for better readability and responsiveness

--- src/components/Blocks/Stats/index.tsx
+++ src/components/Blocks/Stats/index.tsx
@@ -10,15 +10,19 @@ type Props = {
 const StatsSection: FC<Props> = ({ data }) => {
   return (
     <div className="bg-secondary py-5 my-20">
-      <MaxWidthWrapper
-        element="section"
-        className="flex items-center md:justify-between justify-center flex-col md:flex-row gap-10 md:gap-5"
-      >
+      <MaxWidthWrapper element="section" className="grid grid-cols-2 md:grid-cols-4 gap-5">
         {data.stats?.map((stat) => (
-          <div key={stat.id} className="flex items-center flex-col gap-3 justify-center">
-            <Media resource={stat.icon} height={80} width={80} className="size-[80px]" />
-            <h3 className="font-medium text-lg font-body">{stat.label}</h3>
-            <div className="text-xl font-bold tracking-wide">{stat.value}</div>
+          <div key={stat.id} className="flex items-center flex-col justify-center">
+            <Media
+              resource={stat.icon}
+              height={80}
+              width={80}
+              className="size-[55px] lg:size-[80px]"
+            />
+            <div className="mt-3 space-y-1 text-center">
+              <h3 className="font-medium text-base lg:text-lg font-body">{stat.label}</h3>
+              <div className="text-lg lg:text-xl font-bold tracking-wide">{stat.value}</div>
+            </div>
           </div>
         ))}
       </MaxWidthWrapper>
