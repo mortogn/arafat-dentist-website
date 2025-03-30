a9f26ad5e6005abe68af3c9bc946e660efdf5653
Amorto Goon <amorto.goon.121@gmail.com>
2025-03-30 06:03:20 +0600

    feat: enhance StatsSection layout with improved styling and border adjustments

--- src/components/Blocks/Stats/index.tsx
+++ src/components/Blocks/Stats/index.tsx
@@ -9,13 +9,16 @@ type Props = {
 
 const StatsSection: FC<Props> = ({ data }) => {
   return (
-    <div className="bg-primary/70 text-primary-foreground py-8 my-20">
+    <div className="bg-primary/70 text-primary-foreground my-20">
       <MaxWidthWrapper
         element="section"
-        className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-5"
+        className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 lg:divide-x p-0"
       >
         {data.stats?.map((stat) => (
-          <div key={stat.id} className="flex items-center flex-col justify-center">
+          <div
+            key={stat.id}
+            className="flex items-center flex-col justify-center border border-border lg:border-0 p-6"
+          >
             <Media
               resource={stat.icon}
               height={80}
